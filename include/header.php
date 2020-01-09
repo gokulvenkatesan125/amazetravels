7903a4bbec89a8b2b689252cf382c62e7c869fbb
gokul <gokul.venkatesan985@gmai.com>
2020-01-09 10:14:53 +0530

    make the include folders

---
+++ include/header.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="description" content="Peulis | Travel Agency & Tourism HTML Template">
+   <meta name="keyword" content="travel, tourism, agency, tourist">
+   <meta name="author" content="Themescare">
+   <!-- Title -->
+   <title>Amaze Travels</title>
+   <!-- Favicon -->
+   
+   <!--Bootstrap css-->
+   <link rel="stylesheet" href="assets/css/bootstrap.css">
+   <!--Font Awesome css-->
+   <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+   <!--Flaticon css-->
+   <link rel="stylesheet" href="assets/flaticon/flaticon.css">
+   <!--Animatedheadline css-->
+   <link rel="stylesheet" href="assets/css/jquery.animatedheadline.css">
+   <!--Magnific css-->
+   <link rel="stylesheet" href="assets/css/magnific-popup.css">
+   <!--Owl-Carousel css-->
+   <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+   <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+   <!--Animate css-->
+   <link rel="stylesheet" href="assets/css/animate.min.css">
+   <!--Datepicker css-->
+   <link rel="stylesheet" href="assets/css/jquery.datepicker.css">
+   <!--Nice Select css-->
+   <link rel="stylesheet" href="assets/css/nice-select.css">
+   <!--Slicknav css-->
+   <link rel="stylesheet" href="assets/css/slicknav.min.css">
+   <!--Site Main Style css-->
+   <link rel="stylesheet" href="assets/css/style.css">
+   <!--Responsive css-->
+   <link rel="stylesheet" href="assets/css/responsive.css">
+   <script type='text/javascript' src='http://code.jquery.com/jquery-1.11.0.js'></script>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/clockpicker/0.0.7/bootstrap-clockpicker.min.css" integrity="sha256-lBtf6tZ+SwE/sNMR7JFtCyD44snM3H2FrkB/W400cJA=" crossorigin="anonymous" />
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/clockpicker/0.0.7/bootstrap-clockpicker.min.js" integrity="sha256-LPgEyZbedErJpp8m+3uasZXzUlSl9yEY4MMCEN9ialU=" crossorigin="anonymous"></script>
+   
+</head>
+
+<body>
+   <!--Preloader Start-->
+   <div class="preloader">
+      <div class="loading-text"> <span class="loading-text-words" data-preloader="L">L</span>
+         <span class="loading-text-words" data-preloader="O">O</span>
+         <span class="loading-text-words" data-preloader="A">A</span>
+         <span class="loading-text-words" data-preloader="D">D</span>
+         <span class="loading-text-words" data-preloader="I">I</span>
+         <span class="loading-text-words" data-preloader="N">N</span>
+         <span class="loading-text-words" data-preloader="G">G</span>
+      </div>
+   </div>
+   <!--Preloader End-->
+   <!-- Header Area Start -->
+   <header class="peulis-header-area">
+      <!-- Header Top Area Start -->
+      <div class="header-top-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-6 col-sm-6">
+                  <div class="header-top-left">
+                     <p> <i class="fa fa-envelope"></i>
+                        amazetravelhub@gmail.com</p>
+                     <p> <i class="fa fa-phone"></i>
+                        +91 95245 40381</p>
+                  </div>
+               </div>
+               <div class="col-lg-6 col-sm-6">
+                  <div class="header-top-right">
+                     <div class="header-top-social">
+                        <ul>
+                           <li><a href="#"><i class="fa fa-facebook"></i></a>
+                           </li>
+                           <li><a href="#"><i class="fa fa-twitter"></i></a>
+                           </li>
+                           <li><a href="#"><i class="fa fa-pinterest"></i></a>
+                           </li>
+                           <li><a href="#"><i class="fa fa-instagram"></i></a>
+                           </li>
+                        </ul> 
+                     </div>
+                     <div class="header-top-auth">
+                           <ul>
+                              <li><i class="fa fa-user"></i></li>
+                              <li class="separator"><a href="login.php">Login</a></li>
+                              <li><a href="register.php">Register</a></li>
+                           </ul>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Header Top Area End -->
+      <!-- Main Header Area Start -->
+      <div class="main-header-area">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-12">
+                  <div class="header_inn">
+                     <div class="row">
+                        <div class="col-lg-5">
+                           <div class="site-logo">
+                              <a href="index.html">
+                                 <img src="assets/img/logo_1.png" width="100" height="20px" alt="Peulis" />
+                              </a>
+                           </div>
+                           <!-- Responsive Menu Start -->
+                           <div class="peulis-responsive-menu"></div>
+                           <!-- Responsive Menu End -->
+                        </div>
+                        <div class="col-lg-7">
+                           <div class="mainmenu">
+                              <nav>
+                                 <ul id="navigation_menu">
+                                    <li class="active"><a href="index.php">Home</a>
+                                    </li>
+                                    <li> <a href="#">About Us</a>
+                                    </li>
+                                    <li> <a href="tarrif.php">Tarrif</a>
+                                    </li>
+                                    <li> <a href="attachtaxi.php">Attach Taxi</a>
+                                    </li>
+                                    <li><a href="contact.html">Contact Us</a>
+                                    </li>
+                                 </ul>
+                              </nav>
+                           </div>
+                        </div>
+                       <!--  <div class="col-lg-2">
+                           <div class="header_action">
+                              <ul>
+                                 <li class="header-search">
+                                    <a href="#" class="search-btn"> <i class="fa fa-search"></i>
+                                    </a>
+                                    <div class="search-box search-elem"> <span class="search-close"></span>
+                                       <div class="inner row">
+                                          <form>
+                                             <input type="search" placeholder="Type to Search...">
+                                             <button type="submit"><i class="fa fa-search"></i>
+                                             </button>
+                                          </form>
+                                       </div>
+                                    </div>
+                                 </li>
+                                 <li class="header_cart">
+                                    <a href="#"> <span>0</span>
+                                       <i class="fa fa-shopping-bag"></i>
+                                    </a>
+                                    <div class="cart_box_hover">
+                                       <div class="single-cart">
+                                          <a class="product-remove" href="#"> <i class="fa fa-times"></i>
+                                          </a>
+                                          <div class="cart-pro-image">
+                                             <img src="assets/img/cart-1.png" alt="cart" />
+                                          </div>
+                                          <div class="cart-pro-info">
+                                             <a href="#">
+                                                <h4>Tour package 1</h4>
+                                             </a>
+                                             <p>Tickets 2</p>
+                                             <h5>$1650.00</h5>
+                                          </div>
+                                       </div>
+                                       <div class="single-cart">
+                                          <a class="product-remove" href="#"> <i class="fa fa-times"></i>
+                                          </a>
+                                          <div class="cart-pro-image">
+                                             <img src="assets/img/cart-1.png" alt="cart" />
+                                          </div>
+                                          <div class="cart-pro-info">
+                                             <a href="#">
+                                                <h4>Tour package 1</h4>
+                                             </a>
+                                             <p>Tickets 2</p>
+                                             <h5>$1650.00</h5>
+                                          </div>
+                                       </div>
+                                       <div class="single-cart subtotal">
+                                          <p>Subtotal :<span>$3300</span>
+                                          </p>
+                                       </div>
+                                       <div class="cart-action"> <a href="cart.html" class="viewcart">view cart</a>
+                                          <a href="checkout.html" class="checkout-btn">Checkout</a>
+                                       </div>
+                                    </div>
+                                 </li>
+                                 <li><a href="#" id="sidenav-toggle"><i class="fa fa-bars"></i></a>
+                                 </li>
+                              </ul>
+                           </div>
+                        </div> -->
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Main Header Area End -->
+   </header>
+   <!-- Header Area End -->
+   
