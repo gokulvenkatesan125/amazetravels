dcfff0382fd04ab3847c4ffb772f1a290243987f
gokul <gokul.venkatesan985@gmai.com>
2020-01-09 10:14:09 +0530

    rename and include the header and footer in checkout page

---
+++ checkout.php
@@ -0,0 +1,302 @@
+<?php include 'include/header.php';?>
+
+       
+       
+      <!-- Breadcrumb Area Start -->
+      <section class="peulis-breadcrumb-area">
+         <div class="breadcrumb-top">
+            <div class="container">
+               <div class="col-lg-12">
+                  <div class="breadcrumb-box">
+                     <h2>Conform Booking</h2>
+                      <ul class="breadcrumb-inn">
+                        <li><a href="index.php">Home</a></li>
+                        <li class="active"><a href="#">Conform Booking</a></li>
+                      </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- Breadcrumb Area End -->
+       
+       
+      <!-- Checkout Page Area Start -->
+      <section class="checkout-page-area section_70">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-6">
+                  <div class="checkout-left-box">
+                     <h3>Billing Details</h3>
+                     <form>
+                        <div class="row checkout-form">
+                           <div class="col-md-6">
+                              <label for="name23">First Name</label>
+                              <input type="text" name="firstname" id="name23">
+                           </div>
+                           <div class="col-md-6">
+                              <label for="name22">Last Name</label>
+                              <input type="text" name="lastname" id="name22">
+                           </div>
+                        </div>
+                        
+                        
+                        <div class="row checkout-form">
+                           <div class="col-md-12">
+                              <label for="Town2">Town / City *</label>
+                              <input type="text" name="town" id="Town2">
+                           </div>
+                        </div>
+                        <div class="row checkout-form">
+                           <div class="col-md-6">
+                              <label for="info2">Email Address *</label>
+                              <input type="email" name="info2" id="info2">
+                           </div>
+                           <div class="col-md-6">
+                              <label for="info2">Mobile Number *</label>
+                              <input type="text" name="info2" id="info12">
+                           </div>
+                        </div><br>
+                        <h3>Trip Information</h3>
+                        <div class="row checkout-form">
+                           <div class="col-md-6">
+                              <label for="info2">Distance </label>
+                              <input type="email" name="info2" id="info2">
+                           </div>
+                           <div class="col-md-6">
+                              <label for="info2">Travel Time</label>
+                              <input type="text" name="info2" id="info12">
+                           </div>
+                        </div>
+                        <div class="row checkout-form">
+                           <div class="col-md-6">
+                              <label for="info2">Vehicle type </label>
+                              <input type="email" name="info2" id="info2">
+                           </div>
+                           <div class="col-md-6">
+                              <label for="info2">Travel Date</label>
+                              <input type="text" name="info2" id="info12">
+                           </div>
+                        </div>
+                        <div class="row checkout-form">
+                           <div class="col-md-12">
+                              <label for="Town2">Members </label>
+                              <input type="text" name="town" id="Town2">
+                           </div>
+                        </div>
+                     </form>
+                  </div>
+               </div>
+               <div class="col-lg-6">
+                  <div class="order-summury-box">
+                     <h3>Estimated Trip Cost</h3>
+                     <table>
+                        <tbody>
+                           <tr>
+                              <td>Total Cost</td>
+                              <td><input type="text" class="form-control"></td>
+                           </tr>
+                           <tr>
+                              <td>Should Pay 25% of Total</td>
+                               <td><input type="text" class="form-control"></td>
+                           </tr>
+                           <tr>
+                              <td>GST 5 %</td>
+                              <td><input type="text" class="form-control"></td>
+                           </tr>
+                           <tr>
+                              <td>Paying Amount</td>
+                              <td><input type="text" class="form-control"></td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+                  <p><b>Note:</b></p>
+                  <p>The actual bill amount might differ based on actual KMs travelled, hill-station charges, Toll charges & inter-state permits etc.</p><br>
+                  <div class="booking-right">
+                     <div class="peulis-payment clearfix">
+                        <div class="payment">
+                           <input type="radio" id="ss-option" name="selector">
+                           <label for="ss-option">Direct Bank Transfer</label>
+                           <div class="check">
+                              <div class="inside"></div>
+                           </div>
+                           <p>Make your payment directly into our bank account. Please use your Payment ID as the payment reference.</p>
+                        </div>
+                        <div class="payment">
+                           <input type="radio" id="f-option" name="selector">
+                           <label for="f-option">Debit Card Payment</label>
+                           <div class="check">
+                              <div class="inside"></div>
+                           </div>
+                        </div>
+                        <div class="payment">
+                           <input type="radio" id="s-option" name="selector">
+                           <label for="s-option">Credit Card</label>
+                           <div class="check">
+                              <div class="inside"></div>
+                           </div>
+                           <img src="assets/img/master-card.jpg" alt="credit card">
+                        </div>
+                        <div class="payment">
+                           <input type="radio" id="t-option" name="selector">
+                           <label for="t-option">G-pay</label>
+                           <div class="check">
+                              <div class="inside"></div>
+                           </div>
+                           <img width="50" src="assets/img/g-pay.png" alt="credit card">
+                        </div>
+                     </div>
+                     <div class="action-btn">
+                        <a href="#" class="peulis-btn">Pay</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- Checkout Page Area End -->
+       
+       
+      <!-- Footer Area Start -->
+      <footer class="peulis-footer-area">
+         <div class="footer-top-area">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="single-footer">
+                        <p>ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.ipsum dolor sit amet, consectetur </p>
+                        <div class="footer-logo">
+                           <a href="index.html">
+                           <img src="assets/img/footer-logo.png" alt="Peulis" />
+                           </a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="single-footer">
+                        <h3>About Us</h3>
+                        <ul>
+                           <li><a href="#">Why Us</a></li>
+                           <li><a href="#">Why Touring</a></li>
+                           <li><a href="#">Reviews</a></li>
+                           <li><a href="#">Travel Insurance</a></li>
+                           <li><a href="#">Programme</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="single-footer">
+                        <h3>Support</h3>
+                        <ul>
+                           <li><a href="#">Account</a></li>
+                           <li><a href="#">Client Area</a></li>
+                           <li><a href="#">Privacy & Policy</a></li>
+                           <li><a href="#">Author Hangout</a></li>
+                        </ul>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="single-footer">
+                        <h3>Pay Safely</h3>
+                        <p>Eiusmod tempor incididunt utbor etian dolm magna aliqua enim minim</p>
+                        <div class="payment_image">
+                           <img src="assets/img/creditcard-logo.png" alt="Payment Card" />
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="footer-bottom-area">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="footer-bottom-box">
+                        <div class="row">
+                           <div class="col-lg-6 col-sm-6">
+                              <div class="footer-bottom-left">
+                                 <p>&copy; Copyright Peulis - By <a href="#">Themescare</a></p>
+                              </div>
+                           </div>
+                           <div class="col-lg-6 col-sm-6">
+                              <div class="footer-bottom-left">
+                                 <ul>
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
+      <!-- Footer Area End -->
+       
+       
+      <!-- Sidebar Navigation Start -->
+      <div class="ct-sidenav">
+         <button class="close"><i class="fa fa-times"></i></button>
+         <img src="assets/img/logo.png" alt="Site Logo" />
+         <div class="sidenav-info">
+            <p>Lorem Ipsum is simply dummied text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since when an unknown printer took a galley of type and scrambled it to make a type specimen book.printing and typesetting industry It has survived not only five centuries.</p>
+            <ul class="sidenav-contact-info">
+               <li>
+                  <i class="fa fa-map-marker"></i>
+                  <p>29 Division Pt <span>New York, NY 10002, USA</span></p>
+               </li>
+               <li>
+                  <i class="fa fa-phone"></i>
+                  <p> +0 (321) 984 754 <span>Give us a call</span></p>
+               </li>
+               <li>
+                  <i class="fa fa-envelope"></i>
+                  <p> support@example.com<span>24/7 online support</span></p>
+               </li>
+            </ul>
+         </div>
+         <div class="sidenav-social">
+            <h3>Follow Us</h3>
+            <ul>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+         </div>
+      </div>
+      <!-- Sidebar Navigation End -->
+       
+       
+      <!--Jquery js-->
+      <script src="assets/js/jquery.min.js"></script>
+      <!-- Popper JS -->
+      <script src="assets/js/popper.min.js"></script>
+      <!--Bootstrap js-->
+      <script src="assets/js/bootstrap.min.js"></script>
+      <!--Owl-Carousel js-->
+      <script src="assets/js/owl.carousel.min.js"></script>
+      <!--Animatedheadline js-->
+      <script src="assets/js/jquery.animatedheadline.min.js"></script>
+      <!--Slicknav js-->
+      <script src="assets/js/jquery.slicknav.min.js"></script>
+      <!--Magnific js-->
+      <script src="assets/js/jquery.magnific-popup.min.js"></script>
+      <!-- Datepicker -->
+      <script src="assets/js/jquery.datepicker.min.js"></script>
+      <!--Nice Select js-->
+      <script src="assets/js/jquery.nice-select.min.js"></script>
+      <!-- Way Points JS -->
+      <script src="assets/js/waypoints-min.js"></script>
+      <!-- Init JS -->
+      <script src="assets/js/init.js"></script>
+      <!--Main js-->
+      <script src="assets/js/main.js"></script>
+   </body>
+</html>
+
