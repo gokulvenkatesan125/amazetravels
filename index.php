ca2ff220261176421522c92108b6afeab714a149
gokul <gokul.venkatesan985@gmai.com>
2020-01-10 20:19:37 +0530

    changes on index

--- index.php
+++ index.php
@@ -1089,4 +1089,18 @@
    </section>
    <!-- Blog Area End -->
    <!-- Footer Area Start -->
+   <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBmk-LLrIU8jef2wsgV68Yz_tc1qmohh3g&libraries=places"></script>
+   <script>
+        function initialize() {
+          var input = document.getElementById('searchTextField');
+          var autocomplete = new google.maps.places.Autocomplete(input);
+            google.maps.event.addListener(autocomplete, 'place_changed', function () {
+                var place = autocomplete.getPlace();
+                document.getElementById('city2').value = place.name;
+                document.getElementById('cityLat').value = place.geometry.location.lat();
+                document.getElementById('cityLng').value = place.geometry.location.lng();
+            });
+        }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
    <?php include 'include/footer.php';?>
