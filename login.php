dc30362b5d2bb172e00affb2889112b835246d41
gokul <gokul.venkatesan985@gmai.com>
2020-01-10 20:20:11 +0530

    create db connection for user

--- login.php
+++ login.php
@@ -31,20 +31,20 @@
                <div class="col-lg-12">
                   <div class="login-box">
                      <h3>Member Login</h3>
-                     <form>
+                     <form method="post" action="login.php">
                         <p>
-                           <input type="email" placeholder="E-mail Address" />
+                           <input type="email" name="email" placeholder="E-mail Address" />
                            <i class="fa fa-envelope"></i>
                         </p>
                         <p>
-                           <input type="password" placeholder="Password" />
+                           <input type="password" name="password" placeholder="Password" />
                            <i class="fa fa-lock"></i>
                         </p>
                         <p class="lost_pass">
                            <a href="#">Lost your password?</a>
                         </p>
                         <p>
-                           <button type="submit" >login</button>
+                           <button name="submit" type="submit" >login</button>
                         </p>
                      </form>
                      
@@ -61,3 +61,44 @@
        
       <?php include'include/footer.php'
       ?>
+
+<!-- <-------------------------sql connection querys------------------------> 
+   <?php include "admin/config/config.php";
+
+if(isset($_POST["submit"])) {
+
+
+$email=$_POST["email"];
+
+$password=$_POST["password"];
+
+$password=md5($password);
+
+
+$query="select email,password from registration where email ='$email'";
+
+$result=mysqli_query($conn,$query);
+
+while($row= mysqli_fetch_array($result))
+{   
+  $fetched_email=$row['email'] ;
+  $fetched_password= $row['password'] ;    
+}
+echo "fetch email".$fetched_email;
+echo "fetch password".$fetched_password;
+
+if($fetched_email==$email&&$fetched_password==$password)
+{
+   echo"success";
+echo "<script> alert(' Login Successful '); </script>";
+}
+else
+{
+   echo "reject";
+   echo "<script> alert('Sorry,password wrong or Email-id is wrong');
+          
+    </script>";
+}
+mysqli_close($con);
+}
+?>
